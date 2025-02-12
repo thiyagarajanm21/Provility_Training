740b8bc199e5005c01ce633a7f9c11281f7a3add
Provility <provility_mac_17@Provilitys-MacBook-Air.local>
2025-02-12 14:04:58 +0530

    changes in method

--- PythonBasics/methods.py
+++ PythonBasics/methods.py
@@ -1,9 +1,31 @@
 #Methods in python
-#Static Methods
-def func():
-    print("Static Methods")
-func()
+#Instance Method
+print("Instance Method")
+class Company:
+    def __init__(self,emp_name,emp_salary):
+        self.emp_name=emp_name
+        self.emp_salary=emp_salary
+    def display(self):
+        print(f"Employee name : {self.emp_name}\nEmployee Salary : {self.emp_salary}")
+com = Company("Kumar",35000)
+com.display()
 
-def func(para):
-    print(para)
-func(15)
+#Static Method
+print("Static Methods")
+class Car:
+    @staticmethod
+    def drive(name):
+        return f"{name} drive the car"
+print(Car.drive("John"))
+
+#Class Method
+print("Class Method")
+class Bank:
+    ac_holder="Smith"
+    @classmethod
+    def updated(cls,name):
+        cls.ac_holder=name
+print(Bank.ac_holder)
+Bank.updated("Michel")
+print(Bank.ac_holder)
+
