32df32639a14f76693afaa141790aac559b54aee
Provility <provility_mac_17@Provilitys-MacBook-Air.local>
2025-02-12 12:28:58 +0530

    adding concepts

--- PythonBasics/oops.py
+++ PythonBasics/oops.py
@@ -1,3 +1,6 @@
+#Class AND Object
+
+print("--------CLASS AND OBJECT---------")
 class Car:
     def __init__(self,brand,model):
         self.brand=brand
@@ -8,6 +11,7 @@ car1 = Car("Toyota","Camry")
 car1.displaydetails()
 
 #INHERITANCE
+print("--------INHERITANCE---------")
 class Vehicle:
     def __init__(self,brand):
         self.brand=brand
@@ -19,3 +23,57 @@ class Car(Vehicle):
         print(f"Car Brand :{self.brand}\nCar Model :{self.model}")
 c = Car("Hyudai","Creta")
 c.displayinfo()
+
+#Polymorphism
+print("--------POLYMORPHISM---------")
+class Animal:
+    def makesound(self):
+        pass
+class Dog(Animal):
+    def makesound(self):
+        return "bark"
+class Cat(Animal):
+    def makesound(self):
+        return "meow"
+
+a = [Dog(),Cat()]
+for animals in a:
+    print(animals.makesound())
+
+#Encapsulation
+print("--------ENCAPSULATION-------")
+class BankAccount:
+    def __init__(self,balance):
+        self.__balance=balance
+    def deposit(self,amount):
+        self.__balance+=amount
+    def displaybalance(self):
+        return self.__balance
+bank = BankAccount(1000)
+bank.deposit(500)
+print(bank.displaybalance())
+
+#Abstraction
+print("------ABSTRACTION-----")
+from abc import ABC, abstractmethod
+
+class Animal(ABC):
+    @abstractmethod
+    def make_sound(self):
+        pass
+
+class Dog(Animal):
+    def make_sound(self):
+        return "Bark"
+
+class Cat(Animal):
+    def make_sound(self):
+        return "Meow"
+
+animals = [Dog(), Cat()]
+for animal in animals:
+    print(animal.make_sound())
+
+
+
+
