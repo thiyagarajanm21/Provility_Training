b5ad0525b4a46a13deb11ebfedd4f9c522b4045a
thiyagarajanm21 <thiyagu00029@gmail.com>
2026-04-01 17:40:31 +0530

    Two Sum

--- 12022025_thiyagarajan/hello.py
+++ 12022025_thiyagarajan/hello.py
@@ -1,1 +1,13 @@
-print("Hello")
+def two_sum(nums, target):
+    seen = {}  
+    
+    for i, num in enumerate(nums):
+        diff = target - num
+        
+        if diff in seen:
+            return [seen[diff], i]
+        
+        seen[num] = i
+arr = [3, 8, 9, 10, 25]
+tar = 19
+print(two_sum(arr, tar))
